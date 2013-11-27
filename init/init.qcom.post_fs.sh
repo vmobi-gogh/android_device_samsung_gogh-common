dcc9b68e3268183c55b651de0c8d40f8b1291401
chairshot215 <keefe.lehman@gmail.com>
2013-11-26 23:25:50 -0500

    Restore "init.ath3k.bt.sh " to "init.qcom.post_fs.sh" Run Scrip…
    …t list

--- init/init.qcom.post_fs.sh
+++ init/init.qcom.post_fs.sh
@@ -43,6 +43,8 @@ mount -o rw,remount,barrier=1 /system
 /system/bin/sh /system/etc/init.qcom.wifi.sh
 
 # Run BT scripts
+/system/bin/sh /system/etc/init.ath3k.bt.sh
+
 /system/bin/sh /system/etc/init.qcom.bt.sh
 
 # This should be the last command
